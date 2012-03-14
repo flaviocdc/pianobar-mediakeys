8fc7c65376ea6a22381fa94586689d5770aee68b
Flavio Costa <flavio.cdc@gmail.com>
2012-03-14 15:38:38 -0300

    forking before spawing the main dbus loop
    pianobar blocks until the eventcmd returns, forking mitigates that

--- pianobar_event_listener.py
+++ pianobar_event_listener.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-from sys import argv
-from sys import exit
+import sys
+import os
 import dbus_client
 
-if (len(argv) <= 1):
-  exit(0)
+if (len(sys.argv) <= 1):
+  sys.exit(0)
 
-event = argv[1]
+event = sys.argv[1]
 if event == 'userlogin':
-  dbus_client.run();  
+  # have to fork here in order to unblock pianobar
+  # since it waits for the event process to die before proceding
+  pid = os.fork()
+  if pid == 0:
+    dbus_client.run();  
